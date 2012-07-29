e6d38ab573afb20c252980c5a16002eed4dd3cb3
Adam Procter <adamprocter@mac.com>
2012-07-29 21:55:22 +0100

    added some JQ for mobile checking

--- js/application.js
+++ js/application.js
@@ -7,5 +7,19 @@
 $(document).ready(function (){
 
   $.preloadCssImages();
+  
+  var current_width = $(window).width();
+       //do something with the width value here!
+      if(current_width < 400){
+        
+        jQuery('input').attr("data-mini",true);
+        jQuery('a').attr("data-mini",true);
+        jQuery('h2').css("display","none");
+//        jQuery('footer').css("background","x")
+       
+        
+     
+        
+      }
 
 });
