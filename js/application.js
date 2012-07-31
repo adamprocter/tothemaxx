aa68074e397e4661fcf90ad55d8e04ed752d29dc
Adam Procter <adamprocter@mac.com>
2012-07-31 22:15:26 +0100

    updated index/ footers

--- js/application.js
+++ js/application.js
@@ -8,6 +8,19 @@ $(document).ready(function (){
 
   $.preloadCssImages();
   
+//  function startTimer(){
+//    var count = 60;
+//    countdown = setInterval(function(){
+//      $("p.countdown").html(count);
+//      if (count == 0) {
+//        count = 0;
+//      }
+//      count--;
+//    }, 1000);
+//  });
+  
+  
+  
   var current_width = $(window).width();
        //do something with the width value here!
       if(current_width < 400){
