707431e20443d17e56e8fcd0b7da12417a8dce83
Adam Proter <adamprocter@mac.com>
2012-08-15 19:55:05 +0100

    update to version 0.4

--- js/application.js
+++ js/application.js
@@ -1,38 +1,14 @@
 // remap jQuery to $
 (function($){})(window.jQuery);
 
-  
 
 /* trigger when page is ready */
-$(document).ready(function (){
+$(document).on("pageinit", function(event){
 
   $.preloadCssImages();
-  
-//  function startTimer(){
-//    var count = 60;
-//    countdown = setInterval(function(){
-//      $("p.countdown").html(count);
-//      if (count == 0) {
-//        count = 0;
-//      }
-//      count--;
-//    }, 1000);
-//  });
-  
-  
-  
-  var current_width = $(window).width();
-       //do something with the width value here!
-      if(current_width < 400){
-        
-       // jQuery('input').attr("data-mini",true);
-        // jQuery('a').attr("data-mini",true);
-        // jQuery('h2').css("display","none");
-//        jQuery('footer').css("background","x")
-       
-        
-     
+
         
       }
 
 });
+
