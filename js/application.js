309dd58dfe85adb2445e054ccdac4fcb922ed7c4
Adam Proter <adamprocter@mac.com>
2012-08-17 18:09:23 +0100

    revamped version 0.4

--- js/application.js
+++ js/application.js
@@ -1,14 +1,66 @@
 // remap jQuery to $
-(function($){})(window.jQuery);
+//(function($){})(window.jQuery);
 
 
 /* trigger when page is ready */
+
+$('#stuck1').click(function() {
+
+
+  $('#hide1').css("display", "none");
+  
+  });
+
+$('#stuck2').click(function() {
+  $('#hide2').css("display", "none");});
+
+$('#stuck3').click(function() {
+  $('#hide3').css("display", "none");});
+
+$('#stuck4').click(function() {
+  $('#hide4').css("display", "none");});
+
+$('#stuck5').click(function() {
+  $('#hide5').css("display", "none");});
+
+$('#stuck6').click(function() {
+  $('#hide6').css("display", "none");});
+$('#stuck7').click(function() {
+  $('#hide7').css("display", "none");});
+
+$('#stuck8').click(function() {
+  $('#hide8').css("display", "none");});
+
+$('#stuck9').click(function() {
+  $('#hide9').css("display", "none");});
+
+$('#stuck10').click(function() {
+  $('#hide10').css("display", "none");});
+
+$('#stuck11').click(function() {
+  $('#hide11').css("display", "none");});
+
+$('#stuck12').click(function() {
+  $('#hide12').css("display", "none");});
+
+
+$('#stuck13').click(function() {
+  $('#hide13').css("display", "none");});
+
+$('#stuck14').click(function() {
+  $('#hide14').css("display", "none");});
+
+$('#stuck15').click(function() {
+  $('#hide15').css("display", "none");});
+
+$('#stuck16').click(function() {
+  $('#hide16').css("display", "none");});
+
+
 $(document).on("pageinit", function(event){
 
   $.preloadCssImages();
 
-        
-      }
+       
 
 });
-
