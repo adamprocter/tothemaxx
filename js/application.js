502cd8b487e39245bc07ea530ec5bf33caf19420
Adam Procter <adamprocter@mac.com>
2012-09-05 23:33:01 +0100

    removed pushstate false

--- js/application.js
+++ js/application.js
@@ -6,7 +6,6 @@ $(document).on("pageinit", function(event){
 
 $.mobile.buttonMarkup.hoverDelay = 0;
 $.preloadCssImages();
-$.mobile.pushStateEnabled = false;
 
 $('#stuck1').click(function() {
    $('#hide1').css("display", "none");
